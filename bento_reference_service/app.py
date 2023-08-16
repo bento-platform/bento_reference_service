96685d3d8b9eba43ec4ec6c1a856b895e2af29d3
David Lougheed <david.lougheed@gmail.com>
2023-08-16 16:03:23 -0400

    chore!: remove legacy ingest endpoint

--- bento_reference_service/app.py
+++ bento_reference_service/app.py
@@ -5,7 +5,6 @@ from .config import ConfigDependency
 from .constants import BENTO_SERVICE_KIND, SERVICE_TYPE
 from .routers.data_types import data_type_router
 from .routers.genomes import genome_router
-from .routers.ingest import ingest_router
 from .routers.refget import refget_router
 from .routers.schemas import schema_router
 
@@ -16,12 +15,10 @@ app = FastAPI()
 # Attach different routers to the app, for:
 # - data type listing (for Bento-style search)
 # - genome listing
-# - ingest handling
 # - our RefGet API implementation
 # - our JSON schemas
 app.include_router(data_type_router)
 app.include_router(genome_router)
-app.include_router(ingest_router)
 app.include_router(refget_router)
 app.include_router(schema_router)
 

c3e6ceee586e80c020f6ca38616aeb47cee3bafb
David Lougheed <david.lougheed@gmail.com>
2023-11-20 15:33:19 -0500

    chore: add workflow definition for fasta ingest

--- bento_reference_service/workflows/metadata.py
+++ bento_reference_service/workflows/metadata.py
@@ -1,5 +1,32 @@
+from pathlib import Path
+from bento_lib.workflows import models as wm
 from bento_lib.workflows.workflow_set import WorkflowSet
 
 __all__ = ["workflow_set"]
 
-workflow_set = WorkflowSet()
+WORKFLOW_FASTA_REFERENCE = "fasta_ref"
+
+workflow_set = WorkflowSet(Path(__file__).parent / "wdls")
+
+workflow_set.add_workflow(
+    WORKFLOW_FASTA_REFERENCE,
+    wm.WorkflowDefinition(
+        name="Ingest FASTA-formatted reference genome",
+        type="ingestion",
+        description=(
+            "Given a FASTA or gzipped FASTA reference genome, this workflow indexes and ingests it into the Bento "
+            "Reference Service."
+        ),
+        file="fasta_ref",
+        tags=frozenset(("reference", "fasta")),
+        inputs=[
+            # Injected
+            wm.WorkflowSecretInput(id="access_token", key="access_token"),
+            wm.WorkflowServiceUrlInput(id="drs_url", service_kind="drs"),
+            wm.WorkflowServiceUrlInput(id="reference_url", service_kind="reference"),
+            # User-specified
+            wm.WorkflowStringInput(id="genome_id"),
+            wm.WorkflowFileInput(id="genome_fasta", pattern=r"^.*\.(fa|fa.gz|fas|fas.gz|fasta|fasta.gz)$"),
+        ],
+    ),
+)
--- bento_reference_service/workflows/wdls/fasta_ref.wdl
+++ bento_reference_service/workflows/wdls/fasta_ref.wdl
@@ -1,6 +1,6 @@
 version 1.0
 
-workflow ingest_fasta {
+workflow fasta_ref {
     input {
         String genome_id
         File genome_fasta

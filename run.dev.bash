056e81e8542f1b277dc1639c0adf7b5af0676452
David Lougheed <david.lougheed@gmail.com>
2023-12-05 13:40:20 -0500

    fix: dev run script

--- run.dev.bash
+++ run.dev.bash
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-# Set .gitconfig for development
-/set_gitconfig.bash
+# Update dependencies and install module locally
+/poetry_user_install_dev.bash
 
 export ASGI_APP="bento_reference_service.main:app"
 
 # Set default internal port to 5000
 : "${INTERNAL_PORT:=5000}"
 
-python -m poetry install
 python -m debugpy --listen 0.0.0.0:9511 -m \
   uvicorn \
   --host 0.0.0.0 \

44ef1fa4193c82f6631248643c2bc302ba4468eb
David Lougheed <david.lougheed@gmail.com>
2024-05-08 15:24:52 -0400

    fix: misc issues with GFF feature ingestion

--- bento_reference_service/features.py
+++ bento_reference_service/features.py
@@ -1,7 +1,7 @@
 import itertools
 import logging
 import pysam
-import re
+import traceback
 
 from pathlib import Path
 from typing import Generator
@@ -15,7 +15,10 @@ __all__ = [
 ]
 
 
-VALID_GENOME_ID = re.compile(r"^[a-zA-Z0-9\-_.]{3,50}$")
+GFF_CAPTURED_ATTRIBUTES = frozenset({"ID", "Parent"})
+GFF_SKIPPED_FEATURE_TYPES = frozenset({"stop_codon_redefined_as_selenocysteine"})
+GFF_BATCH_SIZE = 5000
+GFF_LOG_PROGRESS_INTERVAL = 1000
 
 
 class AnnotationIngestError(Exception):
@@ -24,27 +27,26 @@ class AnnotationIngestError(Exception):
 
 def parse_attributes(raw_attributes: dict[str, str]) -> dict[str, list[str]]:
     # See "attributes" in http://gmod.org/wiki/GFF3
-    return {k: [url_unquote(e) for e in v.split(",")] for k, v in raw_attributes.items()}
+    return {k: [url_unquote(e) for e in str(v).split(",") if e] for k, v in raw_attributes.items()}
 
 
 def extract_feature_name(record, attributes: dict[str, list[str]]) -> str | None:
-    feature_type = record.feature
+    feature_type = record.feature.lower()
     feature_name: str | None = attributes.get("Name", (None,))[0]
 
     if feature_name:
         return feature_name
 
-    transcript_id = attributes.get("transcript_id", (None,))[0]
-    transcript_name = attributes.get("transcript_name", (transcript_id,))[0]
+    transcript_name = attributes.get("transcript_name", attributes.get("transcript_id", (None,)))[0]
 
     match feature_type:
         case "gene":
             return attributes.get("gene_name", attributes.get("gene_id", (None,)))[0]
         case "transcript":
-            return attributes.get("transcript_name", attributes.get("transcript_id", (None,)))[0]
-        case "5UTR" | "five_prime_utr":  # 5' untranslated region (UTR)
+            return transcript_name
+        case "5utr" | "five_prime_utr":  # 5' untranslated region (UTR)
             return f"{transcript_name} 5' UTR"
-        case "3UTR" | "three_prime_utr":  # 3' untranslated region (UTR)
+        case "3utr" | "three_prime_utr":  # 3' untranslated region (UTR)
             return f"{transcript_name} 3' UTR"
         case "start_codon":
             return f"{transcript_name} start codon"
@@ -55,7 +57,7 @@ def extract_feature_name(record, attributes: dict[str, list[str]]) -> str | None
                 return attributes["exon_id"][0]
             else:
                 return attributes["ID"][0]
-        case "CDS":  # coding sequence
+        case "cds":  # coding sequence
             return f"{transcript_name} CDS"
         case _:
             return None
@@ -82,75 +84,95 @@ async def ingest_gene_feature_annotation(
     :return: None
     """
 
-    # TODO: make sure it's a gtf.gz
-    # TODO: copy it in place
-
     genome: m.GenomeWithURIs | None = await db.get_genome(genome_id)
 
     if genome is None:
         raise AnnotationIngestError(f"Genome with ID {genome_id} not found")
 
-    log_progress_interval = 1000
-
     def _iter_features() -> Generator[m.GenomeFeature, None, None]:
         gff = pysam.TabixFile(str(gff_path), index=str(gff_index_path))
         total_processed: int = 0
+
         try:
             features_by_id: dict[str, m.GenomeFeature] = {}
 
             for contig in genome.contigs:
                 logger.info(f"Indexing features from contig {contig.name}")
-                features_by_id.clear()
 
-                for record in gff.fetch(contig.name, parser=pysam.asGFF3()):
-                    # for some reason, dict(...) returns the attributes dict:
-                    record_attributes = parse_attributes(dict(record))
+                try:
+                    fetch_iter = gff.fetch(contig.name, parser=pysam.asGFF3())
+                except ValueError:
+                    logger.warning(f"Could not find contig with name {contig.name} in GFF3; skipping...")
+                    continue
 
+                for i, record in fetch_iter:
                     feature_type = record.feature
-                    feature_id = record_attributes.get("ID", (None,))[0]
-                    feature_name = extract_feature_name(record, record_attributes)
 
-                    if feature_id is None:
-                        logger.warning(f"Skipping unsupported feature (type={feature_type}, no ID retrieval): {record}")
-                        continue
+                    if feature_type in GFF_SKIPPED_FEATURE_TYPES:
+                        continue  # Don't ingest stop_codon_redefined_as_selenocysteine annotations
 
-                    if feature_name is None:
-                        logger.warning(f"Using ID as name for feature: {record}")
-                        feature_name = feature_id
+                    # for some reason, dict(...) returns the attributes dict:
+                    feature_raw_attributes = dict(record)
 
-                    entry = m.GenomeFeatureEntry(
-                        start_pos=record.start,
-                        end_pos=record.end,
-                        score=record.score,
-                        phase=record.frame,  # misnamed in PySAM's GFF3 parser
-                    )
+                    try:
+                        record_attributes = parse_attributes(feature_raw_attributes)
+                        feature_id = record_attributes.get("ID", (None,))[0]
+                        feature_name = extract_feature_name(record, record_attributes)
 
-                    if feature_id in features_by_id:
-                        features_by_id[feature_id].entries.append(entry)
-                    else:
-                        features_by_id[feature_id] = m.GenomeFeature(
-                            genome_id=genome_id,
-                            contig_name=contig.name,
-                            strand=record.strand,
-                            feature_id=feature_id,
-                            feature_name=feature_name,
-                            feature_type=feature_type,
-                            source=record.source,
-                            entries=[entry],
-                            attributes=record_attributes,
-                            parents=tuple(p for p in record_attributes.get("Parent", "").split(",") if p),
+                        if feature_id is None:
+                            logger.warning(
+                                f"Skipping unsupported feature {i}: type={feature_type}, no ID retrieval; {record}"
+                            )
+                            continue
+
+                        if feature_name is None:
+                            logger.warning(f"Using ID as name for feature {i}: {record}")
+                            feature_name = feature_id
+
+                        entry = m.GenomeFeatureEntry(
+                            start_pos=record.start,
+                            end_pos=record.end,
+                            score=record.score,
+                            phase=record.frame,  # misnamed in PySAM's GFF3 parser
                         )
 
-                total_processed += 1
-                if total_processed % log_progress_interval == 0:
-                    logger.info(f"Processed {total_processed} features")
+                        if feature_id in features_by_id:
+                            features_by_id[feature_id].entries.append(entry)
+                        else:
+                            features_by_id[feature_id] = m.GenomeFeature(
+                                genome_id=genome_id,
+                                contig_name=contig.name,
+                                strand=record.strand,
+                                feature_id=feature_id,
+                                feature_name=feature_name,
+                                feature_type=feature_type,
+                                source=record.source,
+                                entries=[entry],
+                                attributes={
+                                    # skip attributes which have been captured in the above information
+                                    k: v for k, v in record_attributes.items() if k not in GFF_CAPTURED_ATTRIBUTES
+                                },
+                                parents=tuple(p for p in record_attributes.get("Parent", ()) if p),
+                            )
 
-            yield from features_by_id.values()
+                    except Exception as e:
+                        logger.error(
+                            f"Could not process feature {i}: {feature_type=}, {feature_raw_attributes=}; encountered "
+                            f"exception: {e}"
+                        )
+                        logger.error(traceback.format_exc())
+
+                    total_processed += 1
+                    if total_processed % GFF_LOG_PROGRESS_INTERVAL == 0:
+                        logger.info(f"Processed {total_processed} features")
+
+                yield from features_by_id.values()
+                features_by_id.clear()
 
         finally:
             gff.close()
 
     features_to_ingest = _iter_features()
 
-    while data := tuple(itertools.islice(features_to_ingest, 1000)):  # take features in batches
+    while data := tuple(itertools.islice(features_to_ingest, GFF_BATCH_SIZE)):  # take features in batches
         await db.bulk_ingest_genome_features(data)

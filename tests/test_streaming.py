1932d287847e868a3181d689cdc0962dcedacfd1
David Lougheed <david.lougheed@gmail.com>
2024-05-15 13:19:49 -0400

    test: use mock instead of real site for streaming tests

--- tests/test_streaming.py
+++ tests/test_streaming.py
@@ -1,22 +1,23 @@
 import logging
-
 import pytest
 
+from aioresponses import aioresponses
 from fastapi import HTTPException
 
-from bento_reference_service import config as c
-from bento_reference_service import streaming as s
+from bento_reference_service import config as c, streaming as s
 
-HTTP_TEST_URI_1 = "https://hgdownload.soe.ucsc.edu/goldenPath/hg38/chromosomes/md5sum.txt"
+HTTP_TEST_URI = "https://test.local/file.txt"
 
 logger = logging.getLogger(__name__)
 
 
 @pytest.mark.asyncio()
-async def test_http_streaming():
+async def test_http_streaming(aioresponse: aioresponses):
+    aioresponse.get(HTTP_TEST_URI, body=b"test page")
+
     # test that we get back content as expected
-    stream = s.stream_http(c.get_config(), HTTP_TEST_URI_1, {})
-    assert (await anext(stream))[:32] == b"f069c41e7cc8c2d3a7655cbb2d4186b8"  # MD5 sum for chr1
+    stream = s.stream_http(c.get_config(), HTTP_TEST_URI, {})
+    assert (await anext(stream))[:9] == b"test page"
 
     # test that we can consume the entire stream
     async for chunk in stream:
@@ -24,28 +25,29 @@ async def test_http_streaming():
 
 
 @pytest.mark.asyncio()
-async def test_http_streaming_404_1():
+async def test_http_streaming_404_1(aioresponse: aioresponses):
+    aioresponse.get(HTTP_TEST_URI, status=404, body=b"Not Found")
     with pytest.raises(s.StreamingProxyingError):
-        stream = s.stream_http(c.get_config(), "https://hgdownload.soe.ucsc.edu/goldenPath/hg38/DOES_NOT_EXIST", {})
+        stream = s.stream_http(c.get_config(), HTTP_TEST_URI, {})
         await anext(stream)
 
 
 @pytest.mark.asyncio()
-async def test_http_streaming_404_2():
+async def test_http_streaming_404_2(aioresponse: aioresponses):
+    aioresponse.get(HTTP_TEST_URI, status=404, body=b"Not Found")
     with pytest.raises(s.StreamingProxyingError):
-        _, stream = await s.stream_from_uri(
-            c.get_config(), logger, "https://hgdownload.soe.ucsc.edu/goldenPath/hg38/DOES_NOT_EXIST", None, False
-        )
+        _, stream = await s.stream_from_uri(c.get_config(), logger, HTTP_TEST_URI, None, False)
         await anext(stream)
 
 
 @pytest.mark.asyncio()
-async def test_http_streaming_404_3():
+async def test_http_streaming_404_3(aioresponse: aioresponses):
+    aioresponse.get(HTTP_TEST_URI, status=404, body=b"Not Found")
     with pytest.raises(HTTPException):
         res = await s.generate_uri_streaming_response(
             c.get_config(),
             logger,
-            "https://hgdownload.soe.ucsc.edu/goldenPath/hg38/DOES_NOT_EXIST",
+            HTTP_TEST_URI,
             None,
             "text/plain",
             False,

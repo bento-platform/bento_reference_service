de199e38131c1235f3d41da6a3359d2e072975fd
David Lougheed <david.lougheed@gmail.com>
2024-05-21 22:04:12 -0400

    chore(config): increase default file response chunk size

--- bento_reference_service/config.py
+++ bento_reference_service/config.py
@@ -23,7 +23,7 @@ class Config(BentoBaseConfig):
     file_ingest_tmp_dir: Path = Path(__file__).parent.parent / "tmp"  # Default to repository `tmp` folder
     file_ingest_chunk_size: int = 1024 * 256  # 256 KiB at a time
 
-    file_response_chunk_size: int = 1024 * 16  # 16 KiB at a time
+    file_response_chunk_size: int = 1024 * 256  # 256 KiB at a time
     response_substring_limit: int = 10000  # TODO: Refine default
 
     feature_response_record_limit: int = 1000

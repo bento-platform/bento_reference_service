26ae9a21dd01bd3393c6a996b4510250b83a00e5
David Lougheed <david.lougheed@gmail.com>
2024-05-22 10:21:47 -0400

    feat: OpenAPI schema + interactive docs

--- bento_reference_service/authz.py
+++ bento_reference_service/authz.py
@@ -1,3 +1,4 @@
+import re
 from bento_lib.auth.middleware.fastapi import FastApiAuthMiddleware
 from .config import get_config
 from .logger import get_logger
@@ -8,4 +9,11 @@ __all__ = [
 
 # Non-standard middleware setup so that we can import the instance and use it for dependencies too
 config = get_config()  # TODO: Find a way to DI this
-authz_middleware = FastApiAuthMiddleware.build_from_pydantic_config(config, get_logger(config))
+authz_middleware = FastApiAuthMiddleware.build_from_pydantic_config(
+    config,
+    get_logger(config),
+    exempt_request_patterns=(
+        (r"GET", re.escape(config.service_docs_path)),
+        (r"GET", re.escape(config.service_openapi_path)),
+    ),
+)

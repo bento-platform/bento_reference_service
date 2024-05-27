a14c58129059579a5f6bcb95dbe2454e8909a08b
David Lougheed <david.lougheed@gmail.com>
2024-05-27 16:07:48 -0400

    test(refget): bad sequence requests

--- tests/test_refget.py
+++ tests/test_refget.py
@@ -37,15 +37,58 @@ def test_refget_sequence_not_found(test_client: TestClient, db_cleanup):
     assert res.status_code == status.HTTP_404_NOT_FOUND
 
 
+def test_refget_sequence_invalid_requests(test_client: TestClient, aioresponse: aioresponses, db_cleanup):
+    # TODO: fixture
+    create_genome_with_permissions(test_client, aioresponse, TEST_GENOME_SARS_COV_2)
+    test_contig = TEST_GENOME_SARS_COV_2["contigs"][0]
+
+    # ------------------------------------------------------------------------------------------------------------------
+
+    # cannot return HTML
+    res = test_client.get(f"/sequence/{test_contig['md5']}", headers={"Accept": "text/html"})
+    assert res.status_code == status.HTTP_406_NOT_ACCEPTABLE
+    assert res.content == b"Not Acceptable"
+
+    # cannot have start > end
+    res = test_client.get(
+        f"/sequence/{test_contig['md5']}", params={"start": 5, "end": 1}, headers=HEADERS_ACCEPT_PLAIN
+    )
+    assert res.status_code == status.HTTP_416_REQUESTED_RANGE_NOT_SATISFIABLE
+    assert res.content == b"Range Not Satisfiable"
+
+    # start > contig length (by 1 base, since it's 0-based)
+    res = test_client.get(
+        f"/sequence/{test_contig['md5']}", params={"start": test_contig["length"]}, headers=HEADERS_ACCEPT_PLAIN
+    )
+    assert res.status_code == status.HTTP_400_BAD_REQUEST
+    assert res.content == b"Bad Request"
+
+    # end > contig length (by 1 base, since it's 0-based exclusive)
+    res = test_client.get(
+        f"/sequence/{test_contig['md5']}", params={"end": test_contig["length"] + 1}, headers=HEADERS_ACCEPT_PLAIN
+    )
+    assert res.status_code == status.HTTP_400_BAD_REQUEST
+    assert res.content == b"Bad Request"
+
+    # bad range header
+    res = test_client.get(
+        f"/sequence/{test_contig['md5']}", headers={"Range": "dajkshfasd", **HEADERS_ACCEPT_PLAIN}
+    )
+    assert res.status_code == status.HTTP_400_BAD_REQUEST
+    assert res.content == b"Bad Request"
+
+
 def test_refget_sequence_full(test_client: TestClient, aioresponse: aioresponses, db_cleanup):
     # TODO: fixture
     create_genome_with_permissions(test_client, aioresponse, TEST_GENOME_SARS_COV_2)
 
-    test_contig = TEST_GENOME_SARS_COV_2['contigs'][0]
+    test_contig = TEST_GENOME_SARS_COV_2["contigs"][0]
 
     # Load COVID contig bytes
     rf = pysam.FastaFile(str(SARS_COV_2_FASTA_PATH))
     seq = rf.fetch(test_contig["name"]).encode("ascii")
+
+    # ------------------------------------------------------------------------------------------------------------------
 
     # COVID genome should be small enough to fit in the default max-size Refget response, yielding a 200 (not a 206):
     res = test_client.get(f"/sequence/{test_contig['md5']}", headers=HEADERS_ACCEPT_PLAIN)

fee0017cbd491d55481df8688d9d3f7768ca49c4
David Lougheed <david.lougheed@gmail.com>
2024-05-28 10:05:46 -0400

    test(streaming): more http streaming tests

--- tests/test_streaming.py
+++ tests/test_streaming.py
@@ -23,6 +23,30 @@ async def test_http_streaming(aioresponse: aioresponses):
     async for chunk in stream:
         assert isinstance(chunk, bytes)
 
+    # Test with content-length response
+    aioresponse.get(HTTP_TEST_URI, body=b"test page", headers={"content-length": "9"})
+    stream = s.stream_http(c.get_config(), HTTP_TEST_URI, {}, yield_content_length_as_first_8=True)
+    assert (await anext(stream)) == (9).to_bytes(8, byteorder="big")
+    assert (await anext(stream))[:9] == b"test page"
+
+
+@pytest.mark.asyncio()
+async def test_http_streaming_416(aioresponse: aioresponses):
+    aioresponse.get(HTTP_TEST_URI, status=status.HTTP_416_REQUESTED_RANGE_NOT_SATISFIABLE, body=b"Not Satisfiable")
+    with pytest.raises(s.StreamingRangeNotSatisfiable):
+        stream = s.stream_http(c.get_config(), HTTP_TEST_URI, {"Range": "bytes=0-100000"})
+        await anext(stream)
+
+
+@pytest.mark.asyncio()
+async def test_http_streaming_no_content_length(aioresponse: aioresponses):
+    aioresponse.get(HTTP_TEST_URI, body=b"test page")  # doesn't have content-length header in response
+    with pytest.raises(s.StreamingProxyingError):
+        stream = s.stream_http(
+            c.get_config(), HTTP_TEST_URI, {"Range": "bytes=0-100000"}, yield_content_length_as_first_8=True
+        )
+        await anext(stream)
+
 
 @pytest.mark.asyncio()
 async def test_http_streaming_404_1(aioresponse: aioresponses):

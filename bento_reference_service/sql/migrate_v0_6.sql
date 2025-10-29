d55a2a2b84961222042e0720068d39cd71783f08
SanjeevLakhwani <lakhwani.sanjeev691@gmail.com>
2025-10-29 13:19:34 -0400

    reduced verbosity

--- bento_reference_service/sql/migrate_v0_6.sql
+++ bento_reference_service/sql/migrate_v0_6.sql
@@ -1,7 +1,4 @@
 -- Migration (v0.6): widen genome_features.feature_type from VARCHAR(15) -> VARCHAR(31)
-DO $$
-BEGIN
-    ALTER TABLE genome_features
-    ALTER COLUMN feature_type TYPE VARCHAR(31);
-END $$;
+ALTER TABLE genome_features
+ALTER COLUMN feature_type TYPE VARCHAR(31);
 -- End migration (v0.6)
